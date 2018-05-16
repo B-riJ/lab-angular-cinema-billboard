99202b6ef83d16a9b246e29be3e03309cb3f7bef
Brian Jaimes <jaimes.brian001@gmail.com>
2018-05-16 18:41:50 -0400

    iteration 1

--- starter-code/src/app/app.module.ts
+++ starter-code/src/app/app.module.ts
@@ -2,8 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { Routes } from "@angular/router"; // added routes
+import { MovieService } from './movie.service'
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
